5798995a5bb3cc728780e9dc8f6b00c79536f5db
harshlancer <dwivediji169@gmail.com>
2023-06-10 23:24:44 +0530

    new

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Socket } = require("dgram");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+io.on("connection", (socket) => {
+  io.emit("connection");
+  socket.on("disconnection", () => {
+    io.emit("disconnect");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
